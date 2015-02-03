f09b0c52e2dbbe264961f59f1c335096417b0e63
philipp-oldenburg <philipp.oldenburg@stud.unibas.ch>
2015-02-03 15:56:29 +0100

    implemented timing

--- app/src/main/java/oldenburg/philipp/ovenalerter/MainActivityActivity.java
+++ app/src/main/java/oldenburg/philipp/ovenalerter/MainActivityActivity.java
@@ -1,17 +1,52 @@
 package oldenburg.philipp.ovenalerter;
 
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 
 public class MainActivityActivity extends ActionBarActivity {
+
+    private boolean running = false;
+    private int secondsBetweenBeeps = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        final Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                int elapsedTime = (int) ((SystemClock.elapsedRealtime() - chronometer.getBase()) / 1000);
+                if( elapsedTime != 0 && elapsedTime % secondsBetweenBeeps == 0)
+                    Log.d("debug", "" + elapsedTime);
+            }
+        });
+
+        final Button bt_toggleAlert = (Button) findViewById(R.id.bt_toggleAlert);
+        bt_toggleAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!running) {
+                    chronometer.setBase(SystemClock.elapsedRealtime());
+                    chronometer.start();
+                    running = true;
+                    bt_toggleAlert.setText(R.string.bt_stopAlert);
+                } else {
+                    chronometer.stop();
+                    running = false;
+                    bt_toggleAlert.setText(R.string.bt_startAlert);
+                }
+            }
+        });
     }
 
 
